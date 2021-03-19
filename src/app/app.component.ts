7064eea5905fbe50899dbeee56ceb4fe504b825a
Diego Nadler <diego.nadler@foodstuffs.com>
2021-03-19 04:00:17 -0300

    fixing missing html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,64 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-   
+  <!--The content below is only a placeholder and can be replaced.-->
+  <nav-bar></nav-bar>
+  <div class="container-fluid">
+    <div class="row">
+      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <div class="position-sticky pt-3">
+          <ul class="nav flex-column">
+            <li class="nav-item ">
+              <a class="nav-link" routerLink="/socios" routerLinkActive="active">Socios</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" routerLink="/actividades" routerLinkActive="active">Actividades</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link" routerLink="/configuracion" routerLinkActive="active">Configuracion</a>
+            </li>
+          </ul>
+
+          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Saved reports</span>
+            <a class="link-secondary" href="#" aria-label="Add a new report">
+              <span data-feather="plus-circle"></span>
+            </a>
+          </h6>
+          <ul class="nav flex-column mb-2">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Current month
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Last quarter
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Social engagement
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <span data-feather="file-text"></span>
+                Year-end sale
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <router-outlet></router-outlet>
+      </main>
+    </div>
+  </div>
   `,
   styles: ['']
 })
